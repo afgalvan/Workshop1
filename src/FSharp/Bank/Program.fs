3b8956d6e30bd12a2de6e7f1b348e2ef3d139a3b
Andres Galvan <afgalvan@unicesar.edu.co>
2021-08-19 00:02:41 -0500

    Mutate list to preserve state

--- src/FSharp/Bank/Program.fs
+++ src/FSharp/Bank/Program.fs
@@ -10,19 +10,20 @@ module Domain =
 
     type BankAccount(name: string) =
         static let mutable _seed = 1
+        let mutable _transactionHistory : Transaction list = []
         member this.Id = (_seed <- _seed + 1).ToString()
         member this.Owner = name
-        member private this._transactionHistory : Transaction list = []
 
         member private this.ComputeBalance = List.fold (fun x y -> x + y.Amount) 0.0
 
         member this.Balance =
-            this._transactionHistory |> this.ComputeBalance
+            _transactionHistory |> this.ComputeBalance
 
         member private this.AddTransaction(amount: float) =
-            { Amount = amount; Date = DateTime.Now }
-            |> this._transactionHistory.Add
-            |> this.ComputeBalance
+            let transactions = ({ Amount = amount; Date = DateTime.Now }
+            |> _transactionHistory.Add)
+            _transactionHistory <- transactions
+            _transactionHistory |> this.ComputeBalance
 
         member private this.OutOfRange() =
             raise (ArgumentOutOfRangeException "Cantidad a depositar debe ser mayor a 0.")
@@ -49,6 +50,7 @@ module Program =
     let main _ =
         let bankAccount = BankAccount "Rosa"
         bankAccount.Deposit 400.0 |> printfn "%f"
+        bankAccount.Balance |> printfn "Balance $%f"
         bankAccount.Withdraw 100.0 |> printfn "%f"
         bankAccount.Balance |> printfn "Balance $%f"
         Console.ReadKey() |> ignore
